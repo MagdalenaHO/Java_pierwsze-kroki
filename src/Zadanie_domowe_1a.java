d14986708a1b4f85a6095a85dfd7354c1fe94711
MagdalenaHo <m.holyszko7@interia.pl>
2020-11-27 19:44:01 +0100

    Added Zadanie domowe 1a

---
+++ src/Zadanie_domowe_1a.java
@@ -0,0 +1,18 @@
+public class Zadanie_domowe_1a {
+
+    public static void main(String[] args) {
+
+        int firstNumber = 21;
+        int secondNumber =22;
+        int thirdNumber = 15;
+        int forthNumber = 16;
+
+        System.out.println((firstNumber+secondNumber)%2==0);        System.out.println((thirdNumber+forthNumber)%2==0);
+
+        if ((firstNumber+secondNumber)%2==0 && (thirdNumber+forthNumber)%2==0){
+            System.out.println("All numbers are even");
+        }else{
+            System.out.println("All numbers are odd");
+        }
+    }
+}
